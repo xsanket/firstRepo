841c97bcdcae3279c46aced4b7cb591ee636e99b
sanket <sanket@LAPTOP-2RKJKBPL>
2022-12-21 10:48:05 +0530

    addition

--- Day1/src/com/day1/helloDuniya.java
+++ Day1/src/com/day1/helloDuniya.java
@@ -3,6 +3,11 @@ package com.day1;
 public class helloDuniya {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		int a = 20;
+		int b=22;
+		int sum=a+b;
+		
+		System.out.println(sum);
 	}
 
 }
